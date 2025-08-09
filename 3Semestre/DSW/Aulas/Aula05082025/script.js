8f110fadb254849cd0c5b27cd97dc398bf5f6093
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2025-08-09 20:11:00 -0300

    feat: add new code from aula05082025

--- 3Semestre/DSW/Aulas/Aula05082025/script.js
+++ 3Semestre/DSW/Aulas/Aula05082025/script.js
@@ -7,18 +7,19 @@ document.getElementById("btn-carregar").addEventListener("click", () => {
             const info = document.getElementById("info");
 
             info.innerHTML = `
-            <h2>Dados Pessoais</h2>
+            <h3>Dados Pessoais</h3>
             ${dataUser.info.map(item => `
                 <p><strong>Nome:</strong> ${item.nome}</p>
                 <p><strong>Data nascimento:</strong> ${item.nascimento}</p>
                 <p><strong>Email:</strong> ${item.email}</p>
                 <p><strong>Telefone:</strong> ${item.telefone}</p>
                 <p><strong>Linkedin:</strong> ${item.linkedin}</p>
-                <p><strong>Linkedin:</strong> ${item.github}</p>
+                <p><strong>Github:</strong> ${item.github}</p>
                 
                 `)}`;
 
             const experiencia = document.getElementById("experiencia");
+
             experiencia.innerHTML = `
                 <h3>Experiências</h3>
                 ${dataUser.experiencia.map(item => `
@@ -26,12 +27,13 @@ document.getElementById("btn-carregar").addEventListener("click", () => {
                     <p><strong>Empresa:</strong> ${item.empresa}</p>
                     <p><strong>Descrição:</strong> ${item.descricao}</p>
                     <p><strong>Data:</strong> ${item.data}</p>
-                `)}
-            `;
+                    `)}
+                    `;
 
-            const formacao = document.getElementById("formacao").innerHTML = `
-            <h3>Formação</h3>
-            
+            const formacao = document.getElementById("formacao");
+
+            formacao.innerHTML = `
+                    <h3>Formação</h3>
             `;
 
             const conhecimento = document.getElementById("conhecimento").innerHTML = `
