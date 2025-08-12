ba73fef0dab92a23739e80a1675ad3ec4df0e92e
Alexsander Sampaio Lima <alexsandersampaiolima12@gmail.com>
2025-08-12 16:32:25 -0300

    feat: add new code on be1013.java

--- 3Semestre/POO/Aulas/AULA01/aula01.java
+++ 3Semestre/POO/Aulas/AULA01/aula01.java
@@ -1,4 +1,6 @@
+
 public class aula01 {
+
     public static void main(String[] args) {
         Carro c1 = new Carro(2013, "Vermelho", 2.0, "HB20", "Hyundai", true);
         // c1.mostrarInfor();
@@ -6,14 +8,17 @@ public class aula01 {
         // c1.mostrarIdade();
 
         Carro c2 = new Carro(2017, "Prata", 1.6, "WRV", "HONDA", true);
-        c2.mostrarInfor();
-        c2.abastecer();
-        c2.mostrarIdade();
-        c2.carroAnda();
+        // c2.mostrarInfor();
+        // c2.abastecer();
+        // c2.mostrarIdade();
+        // c2.carroAnda();
 
         Carro c3 = new Carro(2003, "Azul", 1.4, "WRV", "HONDA", false);
-        c3.mostrarIdade();
-        c3.mostrarInfor();
+        // c3.mostrarIdade();
+        // c3.mostrarInfor();
+
+        Carro c4 = new Carro(2025, "Preto", 5.0, "I4", "", true);
+        c4.mostrarIdade();
 
         for (int i = 0; i < 11; i++) {
             c1.carroAnda();
