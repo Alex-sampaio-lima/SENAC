7e73c0a75b1125538f226ead4b8c964d6424469d
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2025-08-23 18:08:13 -0300

    feat: Added new functions to the Main.java file to show two ways to the user in Json or HTML format and also adding functions so that the user can have the option to choose

---
+++ 3Semestre/DSW/Aulas/ADO - RevisaoJava/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        String nome;
+        String email;
+        int opcoes = 0;
+
+        do {
+            System.out.println("Digite seu nome completo: ");
+            nome = read.nextLine();
+            System.out.println("Digite seu e-mail principal:");
+            email = read.nextLine();
+
+            System.out.println("0 - Sair");
+            System.out.println("1 - Visualizar no Formato HTML");
+            System.out.println("2 - Visualizar no Formato JSON");
+            System.out.print("Escolha uma das opções: ");
+            opcoes = read.nextInt();
+
+            if (opcoes == 0) {
+                System.out.println("Programa finalizado com sucesso !");
+                break;
+            }
+
+            String html = String.format("<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>Revisão Java </title>\r\n" + //
+                    "</head>\r\n" + //
+                    "\r\n" + //
+                    "<body>\r\n" + //
+                    "    <section>\r\n" + //
+                    "        <table>\r\n" + //
+                    "            <thead>\r\n" + //
+                    "                <th>Informações de Contato</th>\r\n" + //
+                    "            </thead>\r\n" + //
+                    "            <tbody>\r\n" + //
+                    "                <tr>\r\n" + //
+                    "                    <td>Nome: %s</td>\r\n" + //
+                    "                </tr>\r\n" + //
+                    "                <tr>\r\n" + //
+                    "                    <td>Email: %s</td>\r\n" + //
+                    "                </tr>\r\n" + //
+                    "            </tbody>\r\n" + //
+                    "        </table>\r\n" + //
+                    "    </section>\r\n" + //
+                    "</body>\r\n" + //
+                    "\r\n" + //
+                    "</html>", nome, email);
+
+            String json = String.format("{\r\n" + //
+                    "    \"nome\": \"%s\",\r\n" + //
+                    "    \"e-mail\": \"%s\"\r\n" + //
+                    "}\n", nome, email);
+
+            if (opcoes == 1) {
+                System.out.println(html);
+            } else if (opcoes == 2) {
+                System.out.println(json);
+            }
+
+            System.out.println("\n\nDeseja inserir mais um nome ?\nDigite 1 para sim e 2 para não");
+            while (opcoes != 1 || opcoes != 2) {
+                opcoes = read.nextInt();
+            }
+
+            if (opcoes == 2) {
+                break;
+            }
+
+        } while (opcoes != 0);
+
+    };
+};
