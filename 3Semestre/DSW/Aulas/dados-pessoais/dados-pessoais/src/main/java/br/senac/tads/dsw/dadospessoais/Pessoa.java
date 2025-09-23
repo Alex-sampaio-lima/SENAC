54a4ae995127ed257ea811f4bb6c664e12ccd6f6
Alexsander <alexsandersampaiolima12@gmail.com>
2025-09-23 20:28:04 -0300

    feat: add new code

---
+++ 3Semestre/DSW/Aulas/dados-pessoais/dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/Pessoa.java
@@ -0,0 +1,121 @@
+package br.senac.tads.dsw.dadospessoais;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class Pessoa { // POJO
+
+    @NotBlank // @NotNull + NotEmpty
+    @Size(min = 3, max = 64)
+    private String username;
+
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String nome;
+
+    @PastOrPresent
+    private LocalDate dataNascimento;
+
+    @NotBlank
+    @Size(min = 15, max = 100)
+    @Email
+    private String email;
+
+    @Size(max = 20)
+    private String telefone;
+
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.[@$!*%?&][A-Za-z\\d@$!%*?&]).{8,}$")
+    private String senha;
+
+    private String repeticaoSenha;
+
+    @Size(min = 1)
+    private List<String> interesses;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String username, String nome, LocalDate dataNascimento, String email, String telefone, String senha,
+            List<String> interesses) {
+        this.username = username;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.repeticaoSenha = senha;
+        this.interesses = interesses;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<String> getInteresses() {
+        return interesses;
+    }
+
+    public void setInteresses(List<String> interesses) {
+        this.interesses = interesses;
+    }
+
+    public String getRepeticaoSenha() {
+        return repeticaoSenha;
+    }
+
+    public void setRepeticaoSenha(String repeticaoSenha) {
+        this.repeticaoSenha = repeticaoSenha;
+    }
+
+}
