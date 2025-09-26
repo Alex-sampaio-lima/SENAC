15893e5042b9227c2657dc5785b30bae88f33279
ALEXSANDER SAMPAIO LIMA <alexsander.slima@senacsp.edu.br>
2025-09-25 21:59:07 -0300

    feat: add new code

--- 3Semestre/POO/Aulas/Aula25092025/Engenheiro.java
+++ 3Semestre/POO/Aulas/Aula25092025/Engenheiro.java
@@ -6,16 +6,33 @@ public final class Engenheiro extends Profissional {
 
     Engenheiro(Engenheiros especialidade) {
         this.especialidade = especialidade;
+        switch (especialidade) {
+            case Civil:
+                this.salario = 2500;
+                break;
+            case Mecanico:
+                this.salario = 3000;
+                break;
+            case Eletricista:
+                this.salario = 3500;
+                break;
+            case Dados:
+                this.salario = 4000;
+                break;
+            default:
+                this.salario = 2000;
+                break;
+        }
     }
 
     @Override
     public int getSalario() {
-        return 0;
+        return this.salario;
     }
 
     @Override
     public void TipoDeContrato() {
-
+        System.out.println("CLT");
     }
 
     @Override
@@ -33,9 +50,13 @@ public final class Engenheiro extends Profissional {
             case Civil:
                 System.out.println("Fazendo projeto de Casa.");
                 break;
-                default:
+            default:
                 System.out.println("Fazendo projeto.");
         }
+    }
+
+    public Engenheiros getEspecialidade() {
+        return this.especialidade;
     }
 
     public void mostraEspecialiadade() {
